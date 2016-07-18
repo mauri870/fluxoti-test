8ad7a9fe356905a027b96dc7a326d7e08db019e8
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-18 15:47:55 -0300

    Add sketch for inde and showUser

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function showUser($username)
+    {
+        return response()->json(['username' => $username]);
+    }
+}
