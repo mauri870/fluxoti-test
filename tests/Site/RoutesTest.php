d4810bfbb7174e3586448aeef1c33e64850304bc
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-18 17:02:23 -0300

    Add tests for routes

---
+++ tests/Site/RoutesTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Site; 
+
+use App\Tests\TestCase;
+
+class RoutesTest extends TestCase
+{
+    public function test_can_load_index_page()
+    {
+        $this->visit('/')
+        ->assertResponseOk();
+    }
+
+    public function test_can_get_and_cache_user_info()
+    {
+        $response = $this->call('GET', 'user/github');
+        $content = json_decode($response->getContent(), true);
+
+        $this->assertResponseOk();
+        $this->assertArrayHasKey('code', $content);
+        $this->assertArrayHasKey('data', $content);
+
+        $this->assertTrue(app('cache')->has(md5('github')));
+        $this->assertEquals('GitHub', app('cache')->get(md5('github'))['data']['name']);
+
+    }
+}
