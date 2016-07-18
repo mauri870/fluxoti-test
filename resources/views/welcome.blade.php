20ff7a2b1cbb0455c724855d8eea13c19b7eeeb6
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-18 18:54:28 -0300

    Add vendor stylesheet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
 <html>
     <head>
         <title>{{ $title }}</title>
+        <link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
     </head>
     <body>
         <app></app>
     </body>
-    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/vendor.js') }}"></script>
 </html>
