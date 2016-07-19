1456819b42ce92e080712af927b3858533152403
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 10:50:32 -0300

    Simple refactor

--- resources/assets/js/vuex/getters.js
+++ resources/assets/js/vuex/getters.js
@@ -3,7 +3,7 @@ import _ from 'lodash'
 export default {
     user: state => state.user,
 
-    user_exists: state => { return !_.isEmpty(state.user) },
+    user_exists: state => !_.isEmpty(state.user),
 
     search_error_msg: state => state.searchErrorMsg,
 
