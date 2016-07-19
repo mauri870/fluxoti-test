aa2e5b18199e39b9d5f6368849358d82b8dc940f
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 14:56:14 -0300

    Add babel loader and uglifyjs plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path')
-
+var webpack = require('webpack')
 module.exports = {
     resolveLoader: {
         root: path.join(__dirname, '../node_modules'),
@@ -9,6 +9,11 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
             }
         ]
     },
@@ -17,4 +22,20 @@ module.exports = {
             scss: 'style!css!sass'
         }
     }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ])
 }
