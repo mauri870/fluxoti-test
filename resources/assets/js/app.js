9e6bec57082a6444993364e8426a4841627e6a54
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 10:17:23 -0300

    Load vue-resource

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,9 @@
 import Vue from "vue"
+import VueResource from 'vue-resource'
 import App from "./App.vue"
 import store from './vuex'
+
+Vue.use(VueResource)
 
 let vue = new Vue({ store, render: h => h(App) })
 vue.$mount('app')
