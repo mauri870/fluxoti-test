c050dfcd02656c938311984edbf095439812a3be
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 13:51:58 -0300

    Inject the navbar and content components

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,19 @@
+<template lang="jade">
+    div
+        vc-navbar
+
+        vc-content
+</template>
+<script>
+    import VcNavbar from './components/Navbar.vue'
+    import VcContent from './components/Content.vue'
+
+    export default{
+        components: {
+            VcNavbar,
+            VcContent
+        }
+    }
+</script>
+<style>
+</style>
