7f290e1f99a933c775681bd980e2de71d1c332bf
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 11:19:30 -0300

    Add Navbar component

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+    div.navbar.navbar-default.navbar-static-top
+        div.container
+            div.navbar-header
+                button.navbar-toggle(type="button" data-toggle="collapse" data-target=".navbar-collapse")
+                    span.icon-bar
+                    span.icon-bar
+                    span.icon-bar
+                a.navbar-brand(href="#") Github Finder
+</template>
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+<style></style>
