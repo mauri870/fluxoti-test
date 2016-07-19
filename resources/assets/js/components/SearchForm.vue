bfcf3af6ed4c6760781678573ce543abe27c9811
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 10:48:33 -0300

    Add search user form

---
+++ resources/assets/js/components/SearchForm.vue
@@ -0,0 +1,27 @@
+<template lang="jade">
+    form.form-inline(role="form")
+        div.form-group
+            input#username.form-control(v-model="username" type="text" placeholder="username")
+        button.btn.btn-info(@click.prevent="searchUser(username)") Go!
+</template>
+<script>
+    import { mapActions } from 'vuex'
+    export default {
+        name: 'ResultSearch',
+        data () {
+            return {
+                username: ''
+            }
+        },
+        methods: {
+            ...mapActions({
+                searchUser: 'searchUser'
+            })
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    form {
+        margin-top: 10px;
+    }
+</style>
