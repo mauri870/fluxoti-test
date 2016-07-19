b338a9b62dba2ce74eee9f1445d452011b283809
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 13:07:54 -0300

    Show the user profile or an error message

--- resources/assets/js/components/ResultSearch.vue
+++ resources/assets/js/components/ResultSearch.vue
@@ -1,18 +1,35 @@
 <template lang="jade">
     div
-        h1 Result
-
-        button.btn.btn-info(@click.prevent="resetErrorAndUser") Try again!
+        h1 Well, I find this...
+        div.alert.alert-danger(v-if="search_error_exists")
+            h3 Shit, {{ search_error_msg | lowercase }}
+        div(v-else)
+            profile(:user="user")
+        .col-sm-12
+            button.btn.btn-info(@click.prevent="resetErrorAndUser")
+                i.fa.fa-arrow-left
+                |  Try again!
 </template>
 <script>
-    import { mapActions } from 'vuex'
+    import { mapActions, mapGetters } from 'vuex'
+    import Profile from './Profile.vue'
 
     export default {
         name: 'ResultSearch',
+        computed: {
+            ...mapGetters({
+                user: 'user',
+                search_error_msg: 'search_error_msg',
+                search_error_exists: 'search_error_exists',
+            })
+        },
         methods: {
             ...mapActions({
                 resetErrorAndUser: 'resetErrorAndUser'
             })
+        },
+        components: {
+            Profile
         }
     }
 </script>
