61ce23fb1b3df255e9c5227cc189d13d4f5b8ed6
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 13:10:45 -0300

    Add getters and apply condition for show the result or the search form

--- resources/assets/js/components/Content.vue
+++ resources/assets/js/components/Content.vue
@@ -1,19 +1,20 @@
 <template lang="jade">
     div.container
-        div.text-center(v-if="!user_exists")
+        div.text-center(v-if="user_exists || search_error_exists")
+            result-search
+        div.text-center(v-else="")
             h1 Hi!
             h3.lead Need information about a github profile? Give me the username and I will find some things
                 br
             | Just little things, don't expect much '-'
-
             search-form
-        div.text-center(v-else="")
-            result-search
+
 </template>
 <script>
-    import { mapGetters } from 'vuex'
     import ResultSearch from './ResultSearch.vue'
     import SearchForm from './SearchForm.vue'
+
+    import { mapGetters } from 'vuex'
 
     export default {
         name: 'Content',
@@ -23,7 +24,8 @@
         computed: {
             ...mapGetters({
                 user: 'user',
-                user_exists: 'user_exists'
+                user_exists: 'user_exists',
+                search_error_exists: 'search_error_exists',
             })
         },
         components: {
