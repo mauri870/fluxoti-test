91e18e01b304cc833c3cc5a187383020c4179692
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 10:49:02 -0300

    Add search error and user getters

--- resources/assets/js/vuex/getters.js
+++ resources/assets/js/vuex/getters.js
@@ -3,5 +3,9 @@ import _ from 'lodash'
 export default {
     user: state => state.user,
 
-    user_exists: state => { return !_.isEmpty(state.user) }
+    user_exists: state => { return !_.isEmpty(state.user) },
+
+    search_error_msg: state => state.searchErrorMsg,
+
+    search_error_exists: state => state.searchErrorMsg == ''
 }
