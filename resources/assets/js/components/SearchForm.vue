5224ea0860858ad7fda6237240abfc7b330f4b16
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 22:56:57 -0300

    Call the searchUser vuex action if the username contains only letters and numbers

--- resources/assets/js/components/SearchForm.vue
+++ resources/assets/js/components/SearchForm.vue
@@ -3,7 +3,9 @@
         form.form-inline(role="form")
             div.form-group
                 input#username.form-control(v-model="username" type="text" placeholder="username")
-            button.btn.btn-info(@click.prevent="searchUser(username)") Go!
+            button.btn.btn-info(@click.prevent="usernameIsValid && searchUser(username)") Go!
+        .col-sm-6.col-sm-offset-3.alert.alert-danger(v-if="!usernameIsValid")
+            h3 Sorry, but the username can contain only letters and numbers
 </template>
 <script>
     import { mapActions, mapGetters } from 'vuex'
@@ -14,6 +16,11 @@
             return {
                 username: ''
             }
+        },
+        computed: {
+          usernameIsValid () {
+            return this.$options.filters.isAlphanumeric(this.username)
+          }
         },
         methods: {
             ...mapActions({
