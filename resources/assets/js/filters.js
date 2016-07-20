1fbd936fec3e368cc9656b4e687156ecad68e331
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-19 22:56:09 -0300

    Add isAlphanumeric filter

--- resources/assets/js/filters.js
+++ resources/assets/js/filters.js
@@ -3,3 +3,7 @@ import Vue from 'vue'
 Vue.filter('lowercase', (value) => {
     return value.toLowerCase()
 })
+
+Vue.filter('isAlphanumeric', (value) => {
+    return !(/[^a-z\d]/i.test(value))
+})
